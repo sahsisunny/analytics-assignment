5f854449a02c83a0e623924dc4fcaaf69d91f756
Sunny Sahsi <sahsisunny@gmail.com>
2023-12-17 19:53:21 +0530

    change copyright to @

--- client/src/components/footer/index.tsx
+++ client/src/components/footer/index.tsx
@@ -5,7 +5,7 @@ function Footer() {
     <footer className="bg-white border-t-2  py-4">
         <div className="max-w-6xl mx-auto px-6">
             <p className="text-gray-500 text-sm text-center">
-            &copy; 2023 Analytics. All rights reserved.
+            @2023 Analytics. All rights reserved.
             </p>
         </div>
     </footer>
