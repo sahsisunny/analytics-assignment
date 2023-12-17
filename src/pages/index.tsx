73dd8a1990bc7cc741282c7365f7b57550844810
Sunny Sahsi <sahsisunny@gmail.com>
2023-12-17 20:56:48 +0530

    remove uncessary codes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Layout from "../components/Layout";
 import BarChart from "../components/HorizontalBarChart";
-import Chart from 'chart.js/auto';
-import {CategoryScale} from 'chart.js'; 
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
 Chart.register(CategoryScale);
 
 const data = [
@@ -77,22 +77,6 @@ export default function Home() {
   return (
     <Layout>
       <section className="pt-32">
-        <div className="max-w-md mb-6">
-          <h1 className="text-6xl font-bold">
-            Your one link <br />
-            for everything
-          </h1>
-          <h2 className="text-gray-500  text-xl mt-6">
-            Share your profile, social media, and more with a single link.
-          </h2>
-        </div>
-        <form className="inline-flex items-center shadow-lg shadow-gray-500/20">
-          <span className="bg-white py-4 pl-4">linktree.io/</span>
-          <input type="text" className="py-4 " placeholder="username" />
-          <button type="submit" className="bg-blue-500 text-white py-4 px-6">
-            Join Free
-          </button>
-        </form>
         <BarChart data={data} />
       </section>
     </Layout>
